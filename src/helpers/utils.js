3de2381413d7d288b3e53eb49fdfb5c404aba666
Martiros Sahakyan <martiros.sahakyan.1992@gmail.com>
2021-10-06 15:26:46 +0400

    fix(helper): fix undefined case

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,9 +1,9 @@
-export const countCompletionRate =  (arr) => {
-    const allTodos = arr.length
-    const completedTodos = arr.filter((el)=>el.status === 'complete').length 
-    return allTodos ? Math.round(completedTodos / allTodos  * 100) : 0
-}
+export const countCompletionRate = (arr = []) => {
+  const allTodos = arr.length;
+  const completedTodos = arr.filter((el) => el.status === "complete").length;
+  return allTodos ? Math.round((completedTodos / allTodos) * 100) : 0;
+};
 
 export const findUser = (users, id) => {
-    return users.find((user)=>user.id === id)
-}
+  return users.find((user) => user.id === id);
+};
