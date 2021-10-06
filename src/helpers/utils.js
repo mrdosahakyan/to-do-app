48f47bcb62ee28c79d6710ce74b8a75d82485a38
Martiros Sahakyan <martiros.sahakyan.1992@gmail.com>
2021-10-06 11:45:44 +0400

    feat(helpers): add function to find current user

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,6 +1,9 @@
-export function countCompletionRate (arr) {
+export const countCompletionRate =  (arr) => {
     const allTodos = arr.length
     const completedTodos = arr.filter((el)=>el.status === 'complete').length 
     return allTodos ? Math.round(completedTodos / allTodos  * 100) : 0
-    
+}
+
+export const findUser = (users, id) => {
+    return users.find((user)=>user.id === id)
 }
